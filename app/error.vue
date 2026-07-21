abbd3c86ba0f49127a9d008f45528611082be2fa
rahimasalman <rehimesalman7@gmail.com>
2026-07-21 14:57:39 +0400

    feat: add error handling component with multilingual support

---
+++ app/error.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type {NuxtError} from "#app";
+
+defineProps<{
+  error: NuxtError,
+}>()
+const localePath = useLocalePath()
+const handleError = () => {
+  clearError({ redirect: localePath('/') })
+}
+</script>
+
+<template>
+  <div>
+    <h1>Error</h1>
+    <p v-if="error.status === 404 ">
+      {{$t('error.notFound.title') + $t('error.notFound.desc')}}
+    </p>
+    <p v-else>{{$t('error.generic.title') + $t('error.generic.desc')}}</p>
+    <button @click="handleError">{{ $t('error.backHome') }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
